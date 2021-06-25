ef73a04b9ee950a6e0b3eeb1b7af6108ab274089
Raunak Kumar <52267072+Raunakkumarr@users.noreply.github.com>
2021-06-26 00:35:00 +0545

    Add files via upload

---
+++ PDSC-Assignment-3/PDSC-Assignment(Optional1)-3.py
@@ -0,0 +1,12 @@
+import pyautogui
+pyautogui.moveTo(255,1075,duration=2)
+pyautogui.click()
+pyautogui.typewrite('chrome')
+pyautogui.typewrite(['enter'])
+pyautogui.moveTo(970,100,duration=2)
+pyautogui.click()
+pyautogui.typewrite('bit.ly/yt-pdsc')
+pyautogui.typewrite(['enter'])
+pyautogui.moveTo(1330,308,duration=2)
+pyautogui.click()
+print(pyautogui.position())
