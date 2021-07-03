1702d59941c2351a74baf2d45cf6ea0233212c77
Raunak Kumar <52267072+Raunakkumarr@users.noreply.github.com>
2021-07-03 21:56:42 +0545

    Create intro.py

---
+++ resources/day2/intro.py
@@ -0,0 +1,11 @@
+#This code uses maths library to convert the angle in degree to radians and then find the sine of the angle and sys module to close the program automatiocally after the execution is done.
+import math 
+import sys
+#from math import radians as r,sin as s
+
+
+#math module
+rads = math.radians(90)
+print(rads)
+print(math.sin(rads))
+sys.exit()
